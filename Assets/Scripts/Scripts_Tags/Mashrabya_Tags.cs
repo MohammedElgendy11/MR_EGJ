53c6e10406a7486da84bbc09c8757fc171a2a887
MohammedElgendy11 <159758548+chromaversecrew@users.noreply.github.com>
2026-02-07 08:17:40 +0200

    sha3boly rotation

--- Assets/Scripts/Scripts_Tags/Mashrabya_Tags.cs
+++ Assets/Scripts/Scripts_Tags/Mashrabya_Tags.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Mashrabya_Tags : MonoBehaviour
 {
+
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(0f, 180f, 0f);
+
     }
 
     // Update is called once per frame
